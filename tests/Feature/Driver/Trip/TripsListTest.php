466b8017eefefd26142103404642b6679e5e133c
Mykola Ivashchuk <mikola.ivaschuk@yandex.ua>
2017-08-08 16:55:18 +0300

    Added Repository to composer

---
+++ tests/Feature/Driver/Trip/TripsListTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests\Feature\Driver\Trip;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TripsListTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+
+}
