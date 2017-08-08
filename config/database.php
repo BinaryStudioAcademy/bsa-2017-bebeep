f8158cf09840061ac7b84fd97fa871e1eb5d6c5f
rhinodontypicus <rhinodontypicus@gmail.com>
2017-08-08 10:00:59 +0300

    fix

--- config/database.php
+++ config/database.php
@@ -54,21 +54,6 @@ return [
             'engine' => null,
         ],
 
-        'mysql_testing' => [
-            'driver' => 'mysql',
-            'host' => env('DB_TESTING_HOST', '127.0.0.1'),
-            'port' => env('DB_TESTING_PORT', '3306'),
-            'database' => env('DB_TESTING_DATABASE', 'forge'),
-            'username' => env('DB_TESTING_USERNAME', 'forge'),
-            'password' => env('DB_TESTING_PASSWORD', ''),
-            'unix_socket' => env('DB_TESTING_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
