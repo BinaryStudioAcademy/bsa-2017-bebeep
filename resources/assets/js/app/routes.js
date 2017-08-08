c2e135ff3e31185cea1c328ba5f63e81e93c9d92
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-08 20:46:21 +0300

    Changed 404 NotFound route

--- resources/assets/js/app/routes.js
+++ resources/assets/js/app/routes.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import {IndexRoute, Route, Redirect} from 'react-router';
+import { IndexRoute, Route, Redirect } from 'react-router';
 
 import App from './App';
 import Home from './views/Home';
 import Vehicles from '../features/vehicle/views/Vehicles';
 import VehicleOne from '../features/vehicle/views/VehicleOne';
-import NotFoundView from './views/NotFound';
+import NotFound from './views/NotFound';
 
 export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={Home} />
+    <Route path="/" component={ App }>
+        <IndexRoute component={ Home } />
 
-        <Route path="vehicles" component={Vehicles} />
-        <Route path="vehicles/:id" component={VehicleOne} />
+        <Route path="vehicles" component={ Vehicles } />
+        <Route path="vehicles/:id" component={ VehicleOne } />
 
-        <Route path="404" component={NotFoundView} />
-        <Redirect from="*" to="404" />
+        <Route path="*" component={ NotFound } />
     </Route>
 );
