526d5355a0a2099bb07aaf786450db8e29e215c9
Petro Ostapuk <petroostapuk@gmail.com>
2017-08-08 22:14:18 +0300

    init files

---
+++ app/Http/Controllers/Api/Car/CarApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Api\Car;
+
+
+use App\Http\Controllers\Controller;
+
+class CarApiController extends Controller
+{
+    public function create( $request)
+    {
+
+    }
+
+    public function update( $request)
+    {
+
+    }
+
+    public function delete( $request)
+    {
+
+    }
+}
