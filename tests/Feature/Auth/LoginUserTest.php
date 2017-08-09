9833b00f979ed4dd13a5718bcfcdae29f0f04a93
Maxym Kolesnykov <maxkolesnikov93@gmail.com>
2017-08-09 18:59:31 +0300

    base workflow for login test

---
+++ tests/Feature/Auth/LoginUserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginUserTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+
+}
