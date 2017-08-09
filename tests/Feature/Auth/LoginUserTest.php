63c47a9b600f080347b9364157e703bac45962fd
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-08-09 21:06:02 +0300

    added first functional test to loginUserTest

--- tests/Feature/Auth/LoginUserTest.php
+++ tests/Feature/Auth/LoginUserTest.php
@@ -11,5 +11,21 @@ class LoginUserTest extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
 
+    /**
+     * @test
+     */
+    public function guest_cant_auth_if_not_all_required_fields_is_filled()
+    {
+        $response = $this->json('POST', '/api/user/authenticate', []);
+        $response->assertStatus(422);
 
+        $response = $this->json('POST', '/api/user/authenticate', factory(User::class)->make(['email' => null])->toArray());
+        $response->assertStatus(422)->assertJsonStructure(['email' => []]);
+
+        $response = $this->json('POST', '/api/user/authenticate',
+            factory(User::class)->make(['password' => null])->toArray());
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+    }
 }
