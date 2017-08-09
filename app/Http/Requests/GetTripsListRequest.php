31aa54aed8e74193af6629deb63ba00a2b5ee645
Mykola Ivashchuk <mikola.ivaschuk@yandex.ua>
2017-08-09 13:09:39 +0300

    Created validated request for api

---
+++ app/Http/Requests/GetTripsListRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetTripsListRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function validate()
+    {
+        return [
+            'user_id' => 'required|integer'
+        ];
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId():int
+    {
+        return (int)$this->get('user_id');
+    }
+}
