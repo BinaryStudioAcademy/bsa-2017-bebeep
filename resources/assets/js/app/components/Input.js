9f124a9452b3897b9791e14646bbbccb24b643f6
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-10 19:05:43 +0300

    Added profile form, textarea

--- resources/assets/js/app/components/Input.js
+++ resources/assets/js/app/components/Input.js
@@ -11,6 +11,7 @@ class Input extends React.Component {
                            className={"form-control " + (this.props.error ? 'form-control-danger' : '')}
                            id={ this.props.id }
                            name={ this.props.name }
+                           value={ this.props.value }
                            required={ this.props.required }
                            onChange={ this.props.onChange }
                     />
