e3175276a381724cf31677f4d5335c94ab4eaf86
Petro Ostapuk <petroostapuk@gmail.com>
2017-08-11 09:50:09 +0300

    Add new tests

--- tests/Feature/Vehicle/VehicleApiTest.php
+++ tests/Feature/Vehicle/VehicleApiTest.php
@@ -15,4 +15,28 @@ class VehicleApiTest extends TestCase
         $response =  $this->json('GET', self::ENDPOINT);
         $response->assertStatus(200);
     }
+
+    public function test_no_exiting_vehicle(){
+        $response =  $this->json('GET', self::ENDPOINT. '/9999');
+        $response->assertStatus(404);
+    }
+
+    public function test_unecessary_routes()
+    {
+        $response =  $this->json('POST', self::ENDPOINT . '/1');
+        $response->assertStatus(405);
+
+        $response =  $this->json('PATCH', self::ENDPOINT);
+        $response->assertStatus(405);
+
+        $response =  $this->json('DELETE', self::ENDPOINT);
+        $response->assertStatus(405);
+    }
+
+    public function test_header()
+    {
+        $response =  $this->json('GET', self::ENDPOINT );
+        $response->assertHeader('Content-Type', 'application/json');
+    }
+    
 }
