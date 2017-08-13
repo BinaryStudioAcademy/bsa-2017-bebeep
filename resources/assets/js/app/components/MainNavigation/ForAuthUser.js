f1af1a81994ee013f8240fbda0508071b7d2ace9
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-13 20:51:16 +0300

    Set link to Dashboard

--- resources/assets/js/app/components/MainNavigation/ForAuthUser.js
+++ resources/assets/js/app/components/MainNavigation/ForAuthUser.js
@@ -50,7 +50,7 @@ class ForAuthUser extends Component {
 
                   <div className="dropdown-menu dropdown-menu-right">
 
-                    <Link to="#" className="dropdown-item">Dashboard</Link>
+                    <Link to="/dashboard" className="dropdown-item">Dashboard</Link>
                     <Link to="#" className="dropdown-item">My trips</Link>
                     <Link to="#" className="dropdown-item">My bookings</Link>
                     <Link to="#" className="dropdown-item">Profile</Link>
