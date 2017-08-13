f97a26487ed03f4ac85a3542d0f8c2f4339071bd
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-13 16:10:59 +0300

    Added authorization header to api secured request

--- resources/assets/js/app/services/RequestService.js
+++ resources/assets/js/app/services/RequestService.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
+import { getAuthToken } from './AuthService';
+
+const requestParams = {
+    headers: {
+        'Accept': 'application/json',
+    }
+};
 
 export const makeRequest = (method, ...args) => {
     if (typeof axios[method] === 'function') {
+        args.push(requestParams);
+
         return axios[method].apply(this, args);
     }
 };
 
 export const securedRequest = (method, ...args) => {
-    // do something with header (args[2].headers)
+    requestParams.headers['Authorization'] = 'Bearer ' + getAuthToken();
+
     return makeRequest(method, ...args)
 };
 
