2ed56ccf7f26dfbc7ee70c9c4791af30aab6d222
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-08-14 15:04:02 +0300

    index search base dir/file structure created

---
+++ resources/assets/js/features/search/components/SearchForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Input from '../../../app/components/Input';
+import PlacesAutocomplete from 'react-places-autocomplete';
+
+class SearchForm extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default  SearchForm;
+
