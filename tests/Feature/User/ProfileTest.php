3f5014d67b1985921569d92c947223bf9ea8e0f0
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-15 21:13:01 +0300

    Added can_uncheck_role tests

--- tests/Feature/User/ProfileTest.php
+++ tests/Feature/User/ProfileTest.php
@@ -17,6 +17,14 @@ class ProfileTest extends JwtTestCase
     private $routeShow = ['GET'];
     private $routeUpdate = ['PUT'];
 
+    private $driverData = [
+        'first_name' => 'Alex',
+        'last_name' => 'Gartner',
+        'email' => 'alex@example.com',
+        'phone' => '380501112200',
+        'birth_date' => '1984-04-24',
+    ];
+
     private $passengerData = [
         'first_name' => 'Bill',
         'last_name' => 'King',
@@ -24,14 +32,6 @@ class ProfileTest extends JwtTestCase
         'phone' => '380955556633',
         'birth_date' => '1975-08-15',
         'about_me' => 'Lorem ipsum dolor sit amet.',
-    ];
-
-    private $driverData = [
-        'first_name' => 'Alex',
-        'last_name' => 'Gartner',
-        'email' => 'alex@example.com',
-        'phone' => '380501112200',
-        'birth_date' => '1984-04-24',
     ];
 
     public function setUp()
@@ -63,6 +63,25 @@ class ProfileTest extends JwtTestCase
     /**
      * @test
      */
+    public function driver_can_show_profile()
+    {
+        $user = $this->createDriver();
+
+        $response = $this->jsonRequestAsUser($user, $this->routeShow[0], $this->routeShow[1]);
+
+        $response->assertStatus(200)
+             ->assertExactJson(['data' => $this->driverData + [
+                'about_me' => null,
+                'role_driver' => true,
+                'role_passenger' => false,
+                'can_uncheck_driver' => true,
+                'can_uncheck_passenger' => true,
+            ]]);
+    }
+
+    /**
+     * @test
+     */
     public function passenger_can_show_profile()
     {
         $user = $this->createPassenger();
@@ -81,33 +100,113 @@ class ProfileTest extends JwtTestCase
     /**
      * @test
      */
-    public function driver_can_show_profile()
+    public function user_can_uncheck_role_driver_in_profile_form()
     {
-        $user = $this->createDriver();
+        $user = $this->createDriver([
+            'permissions' => User::DRIVER_PERMISSION + User::PASSENGER_PERMISSION,
+        ]);
 
         $response = $this->jsonRequestAsUser($user, $this->routeShow[0], $this->routeShow[1]);
 
         $response->assertStatus(200)
-             ->assertExactJson(['data' => $this->driverData + [
-                'about_me' => null,
-                'role_driver' => true,
-                'role_passenger' => false,
+             ->assertJson(['data' => [
                 'can_uncheck_driver' => true,
                 'can_uncheck_passenger' => true,
             ]]);
     }
 
-    private function createPassenger(): User
+    /**
+     * @test
+     */
+    public function user_can_not_uncheck_role_driver_in_profile_form()
     {
-        return factory(User::class)->create($this->passengerData + [
-            'permissions' => User::PASSENGER_PERMISSION,
+        $user = $this->createDriver([
+            'permissions' => User::DRIVER_PERMISSION + User::PASSENGER_PERMISSION,
+        ]);
+
+        $this->createVehicle();
+        $this->createTrip();
+
+        $response = $this->jsonRequestAsUser($user, $this->routeShow[0], $this->routeShow[1]);
+
+        $response->assertStatus(200)
+             ->assertJson(['data' => [
+                'can_uncheck_driver' => false,
+                'can_uncheck_passenger' => true,
+            ]]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_uncheck_role_passenger_in_profile_form()
+    {
+        $user = $this->createPassenger();
+
+        $response = $this->jsonRequestAsUser($user, $this->routeShow[0], $this->routeShow[1]);
+
+        $response->assertStatus(200)
+             ->assertJson(['data' => [
+                'can_uncheck_driver' => true,
+                'can_uncheck_passenger' => true,
+            ]]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_not_uncheck_role_passenger_in_profile_form()
+    {
+        $user = $this->createPassenger();
+
+        $driver = $this->createDriver();
+        $this->createVehicle($driver->id);
+        $this->createTrip($driver->id);
+        $this->createBooking($user->id);
+
+        $response = $this->jsonRequestAsUser($user, $this->routeShow[0], $this->routeShow[1]);
+
+        $response->assertStatus(200)
+             ->assertJson(['data' => [
+                'can_uncheck_driver' => true,
+                'can_uncheck_passenger' => false,
+            ]]);
+    }
+
+    private function createDriver(array $params = []): User
+    {
+        return factory(User::class)->create($this->driverData + [
+            'id' => 1,
+            'permissions' => $params['permissions'] ?? User::DRIVER_PERMISSION,
         ]);
     }
 
-    private function createDriver(): User
+    private function createPassenger(array $params = []): User
     {
-        return factory(User::class)->create($this->driverData + [
-            'permissions' => User::DRIVER_PERMISSION,
+        return factory(User::class)->create($this->passengerData + [
+            'id' => 2,
+            'permissions' => $params['permissions'] ?? User::PASSENGER_PERMISSION,
+        ]);
+    }
+
+    private function createVehicle(): Vehicle
+    {
+        return factory(Vehicle::class)->create([
+            'user_id' => 1,
+        ]);
+    }
+
+    private function createTrip(): Trip
+    {
+        return factory(Trip::class)->create([
+            'user_id' => 1,
+        ]);
+    }
+
+    private function createBooking(): Booking
+    {
+        return factory(Booking::class)->create([
+            'user_id' => 2,
         ]);
     }
 }
