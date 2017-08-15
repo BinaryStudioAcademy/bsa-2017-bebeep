f7b33a149f859fcf5536c65f0a6bf0faf429dd3d
Sergey B <bondarev.serg@gmail.com>
2017-08-15 21:49:23 +0300

    try connecto to server

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "deploy on production"
-ssh -p $SSH_PASSWORD ssh $SSH_USER@$SSH_HOST <<-EOF
+sshpass -p $SSH_PASSWORD ssh $SSH_USER@$SSH_HOST <<-EOF
 echo $PWD
 exit
 EOF
