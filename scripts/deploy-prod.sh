b000d1b2246e4c8aacae272afa5de2f238a1a9a0
Sergey B <bondarev.serg@gmail.com>
2017-08-15 23:18:46 +0300

    fix path

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "deploy on production"
-echo $pwd
-ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i ../.travis/id_rsa -tt <<-EOF
+echo $PWD
+ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i .travis/id_rsa -tt <<-EOF
 echo $PWD
 mkdir ~/test
 EOF
