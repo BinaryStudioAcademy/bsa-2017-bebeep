f3deedbfbef294fd42ce2e264297965b60e367e8
Sergey B <bondarev.serg@gmail.com>
2017-08-15 22:05:22 +0300

    force tty

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "deploy on production"
-sshpass -p $SSH_PASSWORD ssh $SSH_USER@$SSH_HOST <<-EOF
+sshpass -p $SSH_PASSWORD ssh -tt $SSH_USER@$SSH_HOST <<-EOF
 echo $PWD
 exit
 EOF
