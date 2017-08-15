a3637203a51195dd4f04ca8a22faa9ba30a6c6c3
Sergey B <bondarev.serg@gmail.com>
2017-08-15 22:56:39 +0300

    force tty

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "deploy on production"
-ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i ../.travis/id_rsa <<-EOF
+ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i ../.travis/id_rsa -tt <<-EOF
+echo $PWD
 mkdir ~/test
 EOF
 
