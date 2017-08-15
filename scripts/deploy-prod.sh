296a06cce5e84667bf498689d398d51d5a952deb
Sergey B <bondarev.serg@gmail.com>
2017-08-15 23:43:19 +0300

    production deploy config

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 echo "deploy on production"
-echo $PWD
-ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i .travis/id_rsa -tt <<-EOF
-echo $PWD
-mkdir ~/test
 
+ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i .travis/id_rsa -tt <<-EOF
+cd /srv/production
+git pull
+docker-compose pull
+docker-compose run --rm composer install
+docker-compose run --rm node npm install -q
+docker-compose run --rm node npm run prod
+docker-compose up web -d
 EOF
 
