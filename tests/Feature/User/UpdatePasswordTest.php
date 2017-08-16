b21cddf9bd8fafa5a5d026722638c5964d21b2ad
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-16 09:35:47 +0300

    Completed the user password update tests

--- tests/Feature/User/UpdatePasswordTest.php
+++ tests/Feature/User/UpdatePasswordTest.php
@@ -27,4 +27,85 @@ class UpdatePasswordTest extends JwtTestCase
         $response = $this->json($this->routeUpdate[0], $this->routeUpdate[1]);
         $response->assertStatus(400);
     }
+
+    /**
+     * @test
+     */
+    public function user_can_not_update_password_with_invalid_data()
+    {
+        $currentPassword = 'secret_old';
+        $newPassword = 'secr';
+
+        $user = factory(User::class)->create(['password' => bcrypt($currentPassword)]);
+
+        // Error - the empty data
+        $response = $this->jsonUpdatePassword($user, []);
+        $response->assertStatus(422);
+
+        // Error - the current_password is required
+        $response = $this->jsonUpdatePassword($user, ['current_password' => null]);
+        $response->assertStatus(422)->assertJsonStructure(['current_password' => []]);
+
+        // Error - the password is required
+        $response = $this->jsonUpdatePassword($user, ['password' => null]);
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+
+        // Error - the password_confirmation is required
+        $response = $this->jsonUpdatePassword($user, ['password_confirmation' => null]);
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+
+        // Error - the current_password is not current
+        $response = $this->jsonUpdatePassword($user, ['current_password' => 'qwerty']);
+        $response->assertStatus(422)->assertJsonStructure(['current_password' => []]);
+
+        // Error - the password has not changed (still equals to the current one)
+        $response = $this->jsonUpdatePassword($user, ['password' => $currentPassword]);
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+
+        // Error - the password is not confirmed
+        // (password_confirmation is not equal password)
+        $response = $this->jsonUpdatePassword($user, ['password_confirmation' => 'qwerty']);
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+
+        // Error - the password size is less 6 symbols
+        $response = $this->jsonUpdatePassword($user, ['password' => $newPassword]);
+        $response->assertStatus(422)->assertJsonStructure(['password' => []]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_update_password_with_valid_data()
+    {
+        $currentPassword = 'secret_old';
+        $newPassword = 'secret_new';
+
+        $user = factory(User::class)->create(['password' => bcrypt($currentPassword)]);
+
+        $response = $this->jsonUpdatePassword($user, [
+            'current_password' => $currentPassword,
+            'password' => $newPassword,
+            'password_confirmation' => $newPassword,
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Send request on the user password update.
+     *
+     * @param \App\User $user
+     * @param array $updatedData
+     *
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    private function jsonUpdatePassword(User $user, array $updatedData = [])
+    {
+        return $this->jsonRequestAsUser(
+            $user,
+            $this->routeUpdate[0],
+            $this->routeUpdate[1],
+            $updatedData
+        );
+    }
 }
