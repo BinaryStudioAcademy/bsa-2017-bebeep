07dee0b6551f8af99adea007a4ae214d9cec332f
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-08-16 22:49:33 +0300

    fix Input default values using FORM react key

--- resources/assets/js/features/trip/components/Edit/EditTripForm.js
+++ resources/assets/js/features/trip/components/Edit/EditTripForm.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import Input from '../../../../app/components/Input';
 import PlacesAutocomplete from 'react-places-autocomplete';
+import EditTripService from '../../services/EditTripService';
+import moment from 'moment';
 
 class EditTripForm extends React.Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            trip: {},
+            notFoundTrip: false,
+        };
+    }
+
+    componentDidMount() {
+        const response = {
+            price: 111,
+            seats: 5,
+            start_at: 1502928000,
+        };
+        response.start_at = EditTripService.convertTime(response.start_at);
+        this.setState({
+            trip: response,
+        });
+        /*EditTripService.getTrip()
+            .then(response => {
+                this.setState({
+                    trip: response,
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    notFoundTrip: true,
+                });
+            });*/
     }
 
     render() {
-        const {errors} = this.props;
-
+        const { errors } = this.props;
+        const { trip } = this.state;
         return (
             <form role="form" className="card trip-create-from" action="/api/v1/trips" method=""
-                  onSubmit={this.props.onSubmit}>
+                  onSubmit={this.props.onSubmit} key={moment()}>
                 <div className="card-header">
                     Edit Trip #{this.props.id}
                 </div>
@@ -31,6 +61,7 @@ class EditTripForm extends React.Component {
                         type="number"
                         name="price"
                         id="price"
+                        defaultValue={ trip.price }
                         required={false}
                         error={errors.price}>Price
                     </Input>
@@ -38,6 +69,8 @@ class EditTripForm extends React.Component {
                         type="number"
                         name="seats"
                         id="seats"
+                        defaultValue={ trip.seats }
+                        keys={new Date()}
                         required={false}
                         error={errors.seats}>Available seats
                     </Input>
@@ -67,6 +100,7 @@ class EditTripForm extends React.Component {
                         type="datetime-local"
                         name="start_at"
                         id="start_at"
+                        defaultValue={trip.start_at}
                         required={false}
                         error={errors.start_at}>Trip start time
                     </Input>
--- resources/assets/js/features/trip/services/EditTripService.js
+++ resources/assets/js/features/trip/services/EditTripService.js
@@ -1,13 +1,17 @@
 import { securedRequest } from '../../../app/services/RequestService';
+import moment from 'moment';
 
 const EditTripService = {
     getTrip() {
-        return securedRequest.get('/api/v1/trips')
+        /*return securedRequest.get('/api/v1/trips')
             .then(
                 response => Promise.resolve(response.data),
                 error => Promise.reject(error.response.data)
-            );
+            );*/
     },
+    convertTime(time) {
+        return moment.unix(time).format("YYYY-MM-DDThh:mm");
+    }
 };
 
 export default EditTripService;
