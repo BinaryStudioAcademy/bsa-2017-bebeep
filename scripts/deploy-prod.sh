3cea7581bf4605beaf9eed01846dcd939ce76b29
Sergey B <bondarev.serg@gmail.com>
2017-08-18 00:00:54 +0300

    prod/stage configuration

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -6,10 +6,10 @@ ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i .travis/id_rsa -tt <<-
 cd /srv/production &&
 git pull &&
 docker-compose pull &&
+docker-compose migrate &&
 docker-compose down &&
 docker-compose run --rm composer install &&
 docker-compose run --rm node npm install -q &&
 docker-compose run --rm node npm run prod &&
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d web
 EOF
-
