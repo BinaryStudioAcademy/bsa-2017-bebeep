b6a1d665d558590798448fbc4a31416aea66586e
Sergey B <bondarev.serg@gmail.com>
2017-08-17 19:51:13 +0300

    start docker container for prod

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,12 +3,13 @@
 echo "deploy on production"
 
 ssh -t $SSH_USER@$SSH_HOST -o StrictHostKeyChecking=no -i .travis/id_rsa -tt <<-EOF
-cd /srv/production
-git pull
-docker-compose pull
-docker-compose run --rm composer install
-docker-compose run --rm node npm install -q
-docker-compose run --rm node npm run prod
-docker-compose up web -d
+cd /srv/production &&
+git pull &&
+docker-compose pull &&
+docker-compose down &&
+docker-compose run --rm composer install &&
+docker-compose run --rm node npm install -q &&
+docker-compose run --rm node npm run prod &&
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d web
 EOF
 
