c7b522f26dbea970781202253b8fa655a37ea2e2
Vladimir <lenchvov@rambler.ru>
2017-08-18 18:03:07 +0300

    Fix

--- resources/assets/js/app/components/ChangeLocalization.js
+++ resources/assets/js/app/components/ChangeLocalization.js
@@ -4,13 +4,23 @@ import {bindActionCreators} from 'redux';
 import {setActiveLanguage, getActiveLanguage, getLanguages} from 'react-localize-redux';
 
 class ChangeLocalization extends React.Component {
+    constructor() {
+        super();
+        this.onSetLanguage = this.onSetLanguage.bind(this);
+    }
+
+    onSetLanguage(e) {
+        const langCode = e.target.value;
+        setActiveLanguage(langCode);
+    }
+
     render() {
 
         const {languages, setActiveLanguage, currentLanguage} = this.props;
         return (
             <select className="form-control"
                 defaultValue={currentLanguage}
-                onChange={(e) => setActiveLanguage(e.target.value)}>
+                onChange={this.onSetLanguage}>
                 {languages.map((lang) =>
                     <option key={lang.code} value={lang.code}>{lang.code}</option>
                 )}
