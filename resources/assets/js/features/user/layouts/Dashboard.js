844c103fb3fa13ce704949e9017d2f5eae4c1b41
Vladimir <lenchvov@rambler.ru>
2017-08-19 14:02:52 +0300

    Add dashboard translation

--- resources/assets/js/features/user/layouts/Dashboard.js
+++ resources/assets/js/features/user/layouts/Dashboard.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 
+import LangService from '../../../app/services/LangService';
+import * as lang from '../lang/Dashboard.locale.json';
+import {localize} from 'react-localize-redux';
+
 import PageHeader from '../../../app/components/PageHeader';
 import { getAuthUser } from '../../../app/services/AuthService';
 
 class Dashboard extends Component {
 
+    componentWillMount() {
+        LangService.addTranslation(lang);
+    }
+
     render() {
         const authUser = getAuthUser();
+        const {translate} = this.props;
 
         return (
             <section className="page-section">
-                <PageHeader header={ 'Dashboard' } />
-                <p className="text-center">Hello, { authUser.username }! :-)</p>
+                <PageHeader header={ translate('dashboard.header') } />
+                <p className="text-center">{translate('dashboard.hello', {username: authUser.username})}</p>
             </section>
         )
     }
 }
 
-export default Dashboard;
+export default localize(Dashboard, 'locale');
