7b410f56404f8db636f4aaec5805cd59fb53faf2
Vladimir <lenchvov@rambler.ru>
2017-08-20 15:52:37 +0300

    Translate vehicle page

--- resources/assets/js/features/vehicle/layouts/Vehicles.js
+++ resources/assets/js/features/vehicle/layouts/Vehicles.js
@@ -3,18 +3,28 @@ import React from 'react';
 import PageHeader from 'app/components/PageHeader';
 import VehiclesList from '../components/VehiclesList';
 
+import LangService from 'app/services/LangService';
+import * as lang from '../lang/Vehicles.locale.json';
+import {localize} from 'react-localize-redux';
+
 import '../styles/vehicle.scss';
 
 class Vehicles extends React.Component {
 
+    componentWillMount() {
+        LangService.addTranslation(lang);
+    }
+
     render() {
+        const {translate} = this.props;
+
         return (
             <div>
-                <PageHeader header={ 'Vehicles list' } />
+                <PageHeader header={ translate('vehicles_list_header') } />
                 <VehiclesList />
             </div>
         )
     }
 }
 
-export default Vehicles;
+export default localize(Vehicles, 'locale');
