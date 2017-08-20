b8e6a611e49a8e64e6b1e9a5088eee9fab26fbf7
Vladimir <lenchvov@rambler.ru>
2017-08-20 13:13:55 +0300

    Translate reset password

---
+++ resources/lang/en/Services/PasswordService.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "email_invalid" => "Email is invalid",
+    "token_invalid" => "Token is invalid",
+    "password_invalid" => "Password is invalid",
+    "verify_account" => "Please, verify your account",
+];
---
+++ resources/lang/uk/Services/PasswordService.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "email_invalid" => "Ел. адреса не коректна",
+    "token_invalid" => "Токен не коректний",
+    "password_invalid" => "Пароль не коректний",
+    "verify_account" => "Будь ласка, веріфікуйте свій акаунт.",
+];
