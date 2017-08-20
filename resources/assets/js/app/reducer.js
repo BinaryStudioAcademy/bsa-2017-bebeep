8ffc84d4ff33ad07cb7c5f12b8222ff1493c9714
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-08-20 20:20:04 +0300

    fixed vehicle reducer in app reducer

--- resources/assets/js/app/reducer.js
+++ resources/assets/js/app/reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import user from '../features/user/reducer';
-import vehicle from '../features/vehicle/reducer';
+import vehicle from '../features/car/reducer';
 import trip from '../features/trip/reducer';
 import tripList from '../features/trip-list/reducer';
 import search from '../features/search/reducer';
