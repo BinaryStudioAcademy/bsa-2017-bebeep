74faaf963d268dfec097f323b1a0a83abcb66b6b
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-24 15:44:50 +0300

    Add styles to right sidebar

--- resources/assets/js/features/trip/components/Containers/TripDetailsContainer.js
+++ resources/assets/js/features/trip/components/Containers/TripDetailsContainer.js
@@ -12,7 +12,7 @@ import TripDriver from '../Details/TripDriver';
 import TripVehicle from '../Details/TripVehicle';
 import BookingModal from '../Modals/BookingModal';
 
-//import '../styles/edit_trip.scss';
+import 'features/trip/styles/trip_details.scss';
 
 class TripDetailsContainer extends React.Component {
 
@@ -37,9 +37,13 @@ class TripDetailsContainer extends React.Component {
         TripDetailsService.getDetails(this.props.id)
             .then(response => {
                 response = TripDetailsService.transformData(response.data);
-                console.log(response);
 
                 response.vehicle.data.photo = null;
+
+                response.routes.data[0].busy_seats = 2;
+                response.routes.data[1].busy_seats = 1;
+
+                console.log(response);
 
                 this.setState({
                     trip: response.trip,
@@ -101,7 +105,7 @@ class TripDetailsContainer extends React.Component {
         return (
             <div className="row">
                 <div className="col-md-8">
-                    <div>
+                    <div className="block-border p-3">
                         <div className="trip-main-points">
                             <span className="trip-start-point">
                                 { startPoint.short_address }
@@ -112,7 +116,7 @@ class TripDetailsContainer extends React.Component {
                             </span>
                         </div>
 
-                        <dl className="row trip-main-info">
+                        <dl className="row trip-main-info mt-4">
                             <dt className="col-sm-3">Місце посадки</dt>
                             <dd className="col-sm-9">
                                 <span className="trip-start-point-address">
@@ -143,7 +147,7 @@ class TripDetailsContainer extends React.Component {
                         </div>
                     </div>
 
-                    <section>
+                    <section className="block-border p-3 mt-4">
                         <header>
                             <h3 className="h5">Маршрут і попутники</h3>
                         </header>
@@ -186,51 +190,62 @@ class TripDetailsContainer extends React.Component {
                 </div>
 
                 <div className="col-md-4">
-                    <div className="d-flex">
-                        <div className="trip-price">
-                            <p className="trip-price-value">{ trip.price } &#8372;</p>
-                            <p>з пасажира</p>
+                    <div className="block-border text-center">
+                        <div className="d-flex">
+                            <div className="trip-price px-3 py-2">
+                                <span className="trip-booking-value d-block">{ trip.price } &#8372;</span>
+                                <span className="trip-booking-label">з пасажира</span>
+                            </div>
+                            <div className="trip-places-free px-3 py-2">
+                                <span className="trip-booking-value d-block">2</span>
+                                <span className="trip-booking-label">вільні місця</span>
+                            </div>
                         </div>
-                        <div className="trip-places-free">
-                            <p><span>2</span></p>
-                            <p>вільні місця</p>
+
+                        <div className="trip-passengers p-3">
+                            <span className="trip-booking-label d-block">Пасажири в цій поїздці</span>
+                            <ul className="d-flex flex-wrap justify-content-center list-unstyled mt-3">
+                                <li className="trip-passenger__item">
+                                    <img className="trip-passenger" alt="Юрій К (46 р.)" src="https://d1ovtcjitiy70m.cloudfront.net/vi-1/images/avatar/passenger-male-36.png" />
+                                </li>
+                                <li className="trip-passenger__item">
+                                    <img className="trip-passenger" alt="Юрій К (46 р.)" src="https://d2kwny77wxvuie.cloudfront.net/user/HYg3XtbNS-aOMm3w4AP1cg/thumbnail_36x36.jpeg" />
+                                </li>
+                                <li className="trip-passenger__item">
+                                    <img className="trip-passenger" alt="Юрій К (46 р.)" src="https://d2kwny77wxvuie.cloudfront.net/user/uARSfObaTUqzHDbX_4OBpQ/thumbnail_36x36.jpeg" />
+                                </li>
+                                <li className="trip-passenger__item">
+                                    <img className="trip-passenger" alt="Юрій К (46 р.)" src="https://d1ovtcjitiy70m.cloudfront.net/vi-1/images/avatar/passenger-female-36.png" />
+                                </li>
+                                <li className="trip-passenger__item">
+                                    <span className="trip-passenger trip-passenger--free" />
+                                </li>
+                                <li className="trip-passenger__item">
+                                    <span className="trip-passenger trip-passenger--free" />
+                                </li>
+                            </ul>
                         </div>
-                    </div>
 
-                    <div className="trip-passengers">
-                        <p>Пасажири в цій поїздці</p>
-                        <ul className="list-unstyled">
-                            <li>
-                                <span>1</span>
-                            </li>
-                            <li>
-                                <span>1</span>
-                            </li>
-                            <li>
-                                <span>0</span>
-                            </li>
-                        </ul>
-                    </div>
+                        <div className="trip-booking-button-area p-3">
+                            <button
+                                role="button"
+                                className={"btn trip-booking-button py-3" + (disableBookingBtn ? " disabled" : "")}
+                                onClick={ this.onBookingBtnClick }
+                            >
+                                { translate('trip_booking.booking_btn') }
+                            </button>
 
-                    <div>
-                        <button
-                            role="button"
-                            className={"btn btn-primary" + (disableBookingBtn ? " disabled" : "")}
-                            onClick={ this.onBookingBtnClick }
-                        >
-                            { translate('trip_booking.booking_btn') }
-                        </button>
-
-                        <BookingModal
-                            tripId={ id }
-                            maxSeats={ trip.seats }
-                            waypoints={ routes }
-                            price={ trip.price }
-                            start_at={ 1503774120 * 1000 }
-                            isOpen={ isOpenBookingModal }
-                            onClosed={ this.onBookingClosed }
-                            onSuccess={ this.onBookingSuccess }
-                        />
+                            <BookingModal
+                                tripId={ id }
+                                maxSeats={ trip.seats }
+                                waypoints={ routes }
+                                price={ trip.price }
+                                start_at={ 1503774120 * 1000 }
+                                isOpen={ isOpenBookingModal }
+                                onClosed={ this.onBookingClosed }
+                                onSuccess={ this.onBookingSuccess }
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
