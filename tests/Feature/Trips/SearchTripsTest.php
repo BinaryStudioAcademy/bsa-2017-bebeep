0b9b41981ccd805190e0f1137d96eaa5c7df8a90
Petro Ostapuk <petroostapuk@gmail.com>
2017-08-26 00:52:43 +0300

    Added new test for filter time

--- tests/Feature/Trips/SearchTripsTest.php
+++ tests/Feature/Trips/SearchTripsTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Feature\Trips;
 
+use Carbon\Carbon;
 use App\Models\Vehicle;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class SearchTripsTest extends BaseTripTestCase
 {
+    use DatabaseTransactions;
+
     const ENDPOINT = '/api/v1/trips/search';
 
     protected $method = 'POST';
@@ -14,32 +18,26 @@ class SearchTripsTest extends BaseTripTestCase
         'data' => []
     ];
 
+    public $userId;
+    public $trip;
+
 
     /**
      * Test to find the right data
      */
     public function test_search_exist_data()
     {
-        $user = $this->getDriverUser();
+        $response = $this->create_trip();
 
-        $vehicle = factory(Vehicle::class)->create([
-            'seats' => 4,
-            'user_id' => $user->id,
-        ]);
-
-        $trip = $this->getValidTripData($vehicle->id);
-
-        $createUrl = route('trips.create');
-        $response = $this->jsonRequestAsUser($user, $this->method, $createUrl, $trip);
         $response->assertStatus(200);
 
         $this->assertDatabaseHas(
             'trips',
             [
-                'price' => (float)$trip['price'],
-                'seats' => $trip['seats'],
-                'vehicle_id' => $trip['vehicle_id'],
-                'user_id' => $user->id,
+                'price' => (float)$this->trip['price'],
+                'seats' => $this->trip['seats'],
+                'vehicle_id' => $this->trip['vehicle_id'],
+                'user_id' => $this->userId,
             ]
         );
 
@@ -56,7 +54,7 @@ class SearchTripsTest extends BaseTripTestCase
         $response->assertSee("\"total\":1");
         $response->assertSee("\"seats\":3");
     }
-    
+
     /**
      * Test to find the no right data
      */
@@ -65,6 +63,40 @@ class SearchTripsTest extends BaseTripTestCase
         $response = $this->json('GET', self::ENDPOINT.$search);
         $response->assertStatus(200);
         $response->assertJsonFragment($this->responseNoExistData);
+    }
+
+    public function test_search_filter_time_is_right(){
+        $response = $this->create_trip();
+
+        $response->assertStatus(200);
+        $startAt = Carbon::now()->addHour(1)->timestamp;
+        $hour = Carbon::now()->addHour(3)->hour;
+        $search = '?fc=30.523400000000038|50.4501&start_at='.$startAt.'&tc=36.230383000000074|49.9935&sort=price&order=asc&page=1&limit=10&filter[price][min]=0&filter[price][max]=350&filter[time][min]='.$hour.'&filter[time][max]=24';
+        $response = $this->json('GET', self::ENDPOINT.$search);
+        $response->assertStatus(200);
+//        dd($response);
+        $response->assertSee("350.00");
+        $response->assertSee("\"total\":1");
+        $response->assertSee("\"seats\":3");
+
+    }
+
+    private function create_trip(){
+        $user = $this->getDriverUser();
+
+        $vehicle = factory(Vehicle::class)->create([
+            'seats' => 4,
+            'user_id' => $user->id,
+        ]);
+
+        $trip = $this->getValidTripData($vehicle->id);
+
+        $createUrl = route('trips.create');
+
+        $this->userId = $user->id;
+        $this->trip = $trip;
+
+        return $this->jsonRequestAsUser($user, $this->method, $createUrl, $trip);
     }
 
 }
