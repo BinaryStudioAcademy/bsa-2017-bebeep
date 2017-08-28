13339465b3afd1dc32bb9594d95d3e76dbd446a3
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-28 12:01:36 +0300

    Fixed the redeclaration of the user method in Booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -49,14 +49,6 @@ class Booking extends Model
     }
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    /**
      * Boot the model.
      *
      * @return void
