54e1e2e205aa9d608555b807ecd2c91305fc2bfa
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-08-28 22:09:42 +0300

    styleci

--- app/Transformers/DriverTrip/BookingTransformer.php
+++ app/Transformers/DriverTrip/BookingTransformer.php
@@ -20,7 +20,7 @@ class BookingTransformer extends TransformerAbstract
             'first_name' => $booking->user->first_name,
             'last_name' => $booking->user->last_name,
             'img' => $booking->user->getAvatarUrl(),
-            'user_id' => $booking->user_id
+            'user_id' => $booking->user_id,
         ];
     }
 }
