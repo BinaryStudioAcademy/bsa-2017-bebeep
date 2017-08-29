177188224feaba8b8e819e7d1aa448b43a944a37
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-29 23:55:53 +0300

    Added getPassengerAvatar

--- resources/assets/js/features/trip-list/components/BookingInfo.js
+++ resources/assets/js/features/trip-list/components/BookingInfo.js
@@ -3,18 +3,43 @@ import { localize } from 'react-localize-redux';
 
 import '../styles/booking-info.scss';
 
-export default localize(class BookingInfo extends React.Component {
+class BookingInfo extends React.Component {
+
     render() {
-        const { translate, booking, onApprove, onDecline } = this.props;
+        const { translate, booking, onApprove, onDecline } = this.props,
+            user = booking.user;
+
         return (
             <li className="li-bookings">
                 <div className="row">
-                    <div className="col-md-2"><img src={booking.img} className="img-circle img-booking-user img-responsive" /></div>
-                    <div className="col-md-4"><span>{booking.first_name} {booking.last_name}</span></div>
-                    <div className="col-md-3"><button className="btn btn-sm hover btn-success" onClick={onApprove}>{translate('booking.accept_button')}</button></div>
-                    <div className="col-md-3"><button className="btn btn-sm hover btn-danger" onClick={onDecline}>{translate('booking.reject_button')}</button></div>
+                    <div className="col-md-2">
+                        <img src={user.photo}
+                            className="img-circle img-booking-user img-responsive" />
+                    </div>
+
+                    <div className="col-md-4">
+                        <span>{ user.full_name }</span>
+                    </div>
+
+                    <div className="col-md-3">
+                        <button className="btn btn-sm hover btn-success"
+                            onClick={onApprove}
+                        >
+                            { translate('booking.accept_button') }
+                        </button>
+                    </div>
+
+                    <div className="col-md-3">
+                        <button className="btn btn-sm hover btn-danger"
+                            onClick={onDecline}
+                        >
+                            { translate('booking.reject_button') }
+                        </button>
+                    </div>
                 </div>
             </li>
         );
     }
-}, 'locale');
+}
+
+export default localize(BookingInfo, 'locale');
