725b5f19e436443b1b9e30cdbd19c810826cd6aa
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-08-29 23:36:16 +0300

    Changed transformers, added booking seats

---
+++ app/Transformers/DriverTrip/UserTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Transformers\DriverTrip;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @param \App\User $user
+     * @return array
+     */
+    public function transform(User $user) : array
+    {
+        return [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'avatar' => $user->getAvatarUrl(),
+        ];
+    }
+}
