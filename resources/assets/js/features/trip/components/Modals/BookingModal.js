35c06b9c3bb5135156888460cf2ff81637207ed2
Vladimir <lenchvov@rambler.ru>
2017-09-01 12:59:03 +0300

    Change seats rule

--- resources/assets/js/features/trip/components/Modals/BookingModal.js
+++ resources/assets/js/features/trip/components/Modals/BookingModal.js
@@ -122,7 +122,7 @@ class BookingModal extends React.Component {
     render() {
         const {isOpenModal, freeSeats, errors, start, end} = this.state,
             { translate, waypoints, price, startAt, maxSeats } = this.props;
-
+        console.log(errors);
         return (
             <Modal isOpen={isOpenModal} onClosed={() => { this.closeModal() }}>
                 <form onSubmit={this.onSubmit} className="booking-modal">
@@ -210,6 +210,7 @@ class BookingModal extends React.Component {
                                 </div>
                             </div>
                         </div>
+                        <small className="text-danger">{errors.errors || ''}</small>
                     </div>
                     <div className="modal-footer text-right">
                         <div className="btn btn-danger" role="button" onClick={() => this.closeModal()}>
