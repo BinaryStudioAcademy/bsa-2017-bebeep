531e265c580901e857b17945cffbc4d5398f9b89
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-09-03 01:29:48 +0300

    added errors validation

--- resources/assets/js/features/bookings/components/_Modals/ReviewBookingModal.js
+++ resources/assets/js/features/bookings/components/_Modals/ReviewBookingModal.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { localize } from 'react-localize-redux';
 import Modal from 'app/components/Modal';
 import ReactStars from 'react-stars';
+import Validator from 'app/services/Validator';
+import { createReviewRules } from 'app/services/ReviewService';
 
 class ReviewBookingModal extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            errors: {},
             rating: 0,
             review: ""
         }
@@ -24,12 +27,27 @@ class ReviewBookingModal extends React.Component {
     handleSaveClick(e) {
         e.preventDefault();
 
+        const data = {
+            rating: this.state.rating,
+            review: this.state.review
+        };
+
+        const validated = Validator.validate(createReviewRules(), data);
+
+        if(!validated.valid) {
+            this.setState({errors: validated.errors});
+            return;
+        }
+
+        this.setState({errors: {}});
+
         console.log("Rating", this.state.rating);
         console.log("Review", this.state.review);
     }
 
     render() {
         const {translate} = this.props;
+        const {errors} = this.state;
 
         return (
             <div>
@@ -60,15 +78,16 @@ class ReviewBookingModal extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="form-group">
+                            <div className={"form-group " + (errors.review ? 'has-danger' : '')}>
                                 <label htmlFor="message-text" className="form-control-label">
                                     <strong>{ translate('bookings_list.review') }</strong>
                                 </label>
-                                <textarea className="form-control"
+                                <textarea className={"form-control " + (errors.review ? 'form-control-danger' : '')}
                                           id="message-text"
                                           placeholder={ translate('bookings_list.leave_review_placeholder') }
                                           onChange={this.reviewChanged.bind(this)}
                                 />
+                                <div className="form-control-feedback">{ errors.review }</div>
                             </div>
                         </form>
                     </div>
