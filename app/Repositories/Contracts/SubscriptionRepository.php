e3886721a48cb634f97a9bb93dffcb696a2137ed
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-09-05 00:04:29 +0300

    created basic subscription functional: - migration; - model; - repository; - service;

---
+++ app/Repositories/Contracts/SubscriptionRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+use Prettus\Repository\Contracts\RepositoryCriteriaInterface;
+
+interface SubscriptionRepository extends RepositoryInterface, RepositoryCriteriaInterface
+{
+
+}
