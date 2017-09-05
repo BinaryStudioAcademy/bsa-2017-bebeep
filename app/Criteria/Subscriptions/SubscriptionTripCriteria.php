35a9ef1dbd3c1300c091b0dae4b7e85bc838470c
Vladimir <lenchvov@rambler.ru>
2017-09-05 21:42:16 +0300

    Added criteria search subscription

--- app/Criteria/Subscriptions/SubscriptionTripCriteria.php
+++ app/Criteria/Subscriptions/SubscriptionTripCriteria.php
@@ -3,6 +3,8 @@
 namespace App\Criteria\Subscriptions;
 
 use App\Models\Trip;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\RepositoryInterface;
 
@@ -27,7 +29,36 @@ class SubscriptionTripCriteria implements CriteriaInterface
      */
     public function apply($model, RepositoryInterface $repository)
     {
-        return $model;
+        /** @var Carbon $startAt */
+        $startAt = $this->trip->start_at;
+        /** @var Builder $model */
+        $query = $model
+            ->select('subscriptions.*')
+            ->leftJoin('filters', 'filters.subscription_id', '=','subscriptions.id')
+            ->whereRaw('(SELECT '.
+                $this->haversine(
+                    'routes.from_lat',
+                    'routes.from_lng',
+                    'subscriptions.from_lat',
+                    'subscriptions.from_lng'
+                ).' '.
+                'FROM routes '.
+                'WHERE trip_id='.$this->trip->id.' AND trip_id IS NOT NULL) < '.self::DISTANCE_FROM
+            )
+            ->whereRaw('(SELECT '.
+                $this->haversine(
+                    'routes.to_lat',
+                    'routes.to_lng',
+                    'subscriptions.to_lat',
+                    'subscriptions.to_lng'
+                ).' '.
+                'FROM routes '.
+                'WHERE trip_id='.$this->trip->id.' AND trip_id IS NOT NULL) < '.self::DISTANCE_TO
+            )
+            ->whereDate('subscriptions.start_at', '=', $startAt->toDateString())
+            ->where('subscriptions.is_active', true);
+
+        return $query;
     }
 
     /**
@@ -35,16 +66,16 @@ class SubscriptionTripCriteria implements CriteriaInterface
      *
      * @param string $startLat
      * @param string $startLng
-     * @param float $endLat
-     * @param float $endLng
+     * @param string $endLat
+     * @param string $endLng
      * @return string
      */
-    private function haversine(string $startLat, string $startLng, float $endLat, float $endLng): string
+    private function haversine(string $startLat, string $startLng, string $endLat, string $endLng): string
     {
         return 'ROUND(2 * ' . self::EARTH_RADIUS_KM . ' * ASIN(SQRT( '.
-            'POWER(SIN(RADIANS({$startLat} - {$endLat}) / 2), 2) + '.
-            'COS(RADIANS({$startLat})) * COS(RADIANS($endLat)) * '.
-            'POWER(SIN(RADIANS({$startLng} - $endLng) / 2), 2) '.
+            "POWER(SIN(RADIANS({$startLat} - {$endLat}) / 2), 2) + ".
+            "COS(RADIANS({$startLat})) * COS(RADIANS($endLat)) * ".
+            "POWER(SIN(RADIANS({$startLng} - $endLng) / 2), 2) ".
             ')), 1)';
     }
 }
