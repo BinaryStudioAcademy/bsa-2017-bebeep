a0e208744c75ab09bb67359d84fce805db99f282
Vladimir <lenchvov@rambler.ru>
2017-09-06 12:32:15 +0300

    Fix soft delete

---
+++ app/Services/Helpers/Subscriptions/Contracts/SubscriptionFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Helpers\Subscriptions\Contracts;
+
+use App\Models\Trip;
+use App\Models\Filter;
+
+interface SubscriptionFilter
+{
+    /**
+     * Is the filter supported.
+     *
+     * @param Filter $filter
+     * @return bool
+     */
+    public function support(Filter $filter) : bool;
+
+    /**
+     * @param Filter $filter
+     * @param Trip $trip
+     * @return bool
+     */
+    public function isSatisfied(Filter $filter, Trip $trip) : bool;
+}
