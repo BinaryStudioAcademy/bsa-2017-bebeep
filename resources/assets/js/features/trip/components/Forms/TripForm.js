ab4da953fe1f2e4a2ffb7ec9b6b9bd55a96ea301
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-06 16:50:11 +0300

    Fixed start_at, changed code

--- resources/assets/js/features/trip/components/Forms/TripForm.js
+++ resources/assets/js/features/trip/components/Forms/TripForm.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import { localize } from 'react-localize-redux';
+import moment from 'moment';
 
 import Waypoints from './Waypoints';
 import Input from 'app/components/Input';
 import { InputDateTime } from 'app/components/Controls';
+
 import { getVehicles } from 'features/car/actions';
-import moment from 'moment';
 import LangService from 'app/services/LangService';
 import * as lang from 'features/trip/lang/TripForm.locale.json';
 
@@ -23,40 +24,61 @@ class TripForm extends React.Component {
         return current.isAfter(moment().subtract(1, 'day'));
     }
 
-    render() {
-        const { errors, translate } = this.props;
-        let trip = this.props.trip || {
-            is_animals_allowed: false,
-            luggage_size: 2,
-            vehicle_id: '',
-            price: '',
-            seats: '',
-            start_at: ''
-        };
+    renderVehiclesList() {
+        const { vehicles, trip } = this.props,
+            defaultValue = trip ? trip.vehicle_id : '';
+
+        if (vehicles.length <= 0) {
+            return null;
+        }
 
         return (
-            <form role="form" className="card trip-create-from" action="/api/v1/trips" method="POST"
-                  onSubmit={ this.props.onSubmit }>
-                <div className="card-header">
-                    {this.props.trip ?
-                        translate('trip_form.edit_trip') :
-                        translate('trip_form.create_trip')
-                    }
-                </div>
+            <select name="vehicle_id"
+                id="vehicle_id"
+                className="form-control"
+                defaultValue={defaultValue}
+            >
+                {vehicles.map((vehicle) =>
+                    <option key={vehicle.id} value={vehicle.id}>
+                        {vehicle.brand} {vehicle.model}</option>
+                )}
+            </select>
+        );
+    }
+
+    render() {
+        const { errors, translate, trip, waypoints, startPoint, endPoint, placesCssClasses,
+                onSubmit, onSelectStartPoint, onSelectEndPoint, onWaypointAdd, onWaypointDelete
+            } = this.props;
+
+        const tripData = trip || {
+                is_animals_allowed: false,
+                luggage_size: 2,
+                vehicle_id: '',
+                price: '',
+                seats: '',
+                start_at: ''
+            },
+            formHeader = translate(trip ? 'trip_form.edit_trip' : 'trip_form.create_trip'),
+            buttonText = translate(trip ? 'trip_form.edit_trip_btn' : 'trip_form.create_trip_btn');
+
+        return (
+            <form role="form"
+                method="POST"
+                action="/api/v1/trips"
+                className="card trip-create-from"
+                onSubmit={onSubmit}
+            >
+                <div className="card-header">{formHeader}</div>
                 <div className="card-block">
                     <div className={"form-group row " + (errors.vehicle_id ? 'has-danger' : '')}>
-                        <label className="form-control-label text-muted col-sm-4" htmlFor="vehicle_id">
+                        <label className="form-control-label text-muted col-sm-4"
+                            htmlFor="vehicle_id"
+                        >
                             {translate('trip_form.select_car')}
                         </label>
                         <div className="col-sm-8">
-                            {this.props.vehicles.length > 0 &&
-                                <select defaultValue={trip.vehicle_id} name="vehicle_id" className="form-control" id="vehicle_id">
-                                    {this.props.vehicles.map((vehicle) =>
-                                        <option key={vehicle.id} value={vehicle.id}>
-                                            {vehicle.brand} {vehicle.model}</option>
-                                    )}
-                                </select>
-                            }
+                            {this.renderVehiclesList()}
                             <div className="form-control-feedback">{errors.vehicle_id}</div>
                         </div>
                     </div>
@@ -64,49 +86,62 @@ class TripForm extends React.Component {
                         type="number"
                         name="price"
                         id="price"
-                        defaultValue={trip.price}
+                        defaultValue={tripData.price}
                         required={false}
-                        error={errors.price}>{translate('trip_form.price')}
+                        error={errors.price}
+                    >
+                        {translate('trip_form.price')}
                     </Input>
                     <Input
                         type="number"
                         name="seats"
                         id="seats"
-                        defaultValue={trip.seats}
+                        defaultValue={tripData.seats}
                         required={false}
-                        error={errors.seats}>{translate('trip_form.available_seats')}
+                        error={errors.seats}
+                    >
+                        {translate('trip_form.available_seats')}
                     </Input>
-                    <div className={"form-group row " + (this.props.errors.from ? 'has-danger' : '')}>
-                        <label className="form-control-label text-muted col-sm-4">{translate('trip_form.start_point')}</label>
+                    <div className={"form-group row " + (errors.from ? 'has-danger' : '')}>
+                        <label className="form-control-label text-muted col-sm-4">
+                            {translate('trip_form.start_point')}
+                        </label>
                         <div className="col-sm-8">
-                            <PlacesAutocomplete inputProps={this.props.startPoint}
-                                                classNames={this.props.placesCssClasses}
-                                                onSelect={this.props.onSelectStartPoint}
-                                                onEnterKeyDown={this.props.onSelectStartPoint}
+                            <PlacesAutocomplete
+                                inputProps={startPoint}
+                                classNames={placesCssClasses}
+                                onSelect={onSelectStartPoint}
+                                onEnterKeyDown={onSelectStartPoint}
                             />
-                            <div className="form-control-feedback">{this.props.errors.from}</div>
+                            <div className="form-control-feedback">{errors.from}</div>
                         </div>
                     </div>
-                    <div className={"form-group row " + (this.props.errors.to ? 'has-danger' : '')}>
-                        <label className="form-control-label text-muted col-sm-4">{translate('trip_form.end_point')}</label>
+                    <div className={"form-group row " + (errors.to ? 'has-danger' : '')}>
+                        <label className="form-control-label text-muted col-sm-4">
+                            {translate('trip_form.end_point')}
+                        </label>
                         <div className="col-sm-8">
-                            <PlacesAutocomplete inputProps={this.props.endPoint}
-                                                classNames={this.props.placesCssClasses}
-                                                onSelect={this.props.onSelectEndPoint}
-                                                onEnterKeyDown={this.props.onSelectEndPoint}
+                            <PlacesAutocomplete inputProps={endPoint}
+                                classNames={placesCssClasses}
+                                onSelect={onSelectEndPoint}
+                                onEnterKeyDown={onSelectEndPoint}
                             />
-                            <div className="form-control-feedback">{this.props.errors.to}</div>
+                            <div className="form-control-feedback">{errors.to}</div>
                         </div>
                     </div>
 
-                    <div className={ "form-group row " + (errors.start_at ? 'has-danger' : '') }>
-                        <label htmlFor='start_at' className='form-control-label text-muted col-sm-4'>{translate('trip_form.trip_start_time')}</label>
+                    <div className={"form-group row " + (errors.start_at ? 'has-danger' : '')}>
+                        <label className="form-control-label text-muted col-sm-4"
+                            htmlFor="start_at"
+                        >
+                            {translate('trip_form.trip_start_time')}
+                        </label>
                         <div className="col-md-8">
                             <InputDateTime
                                 id="start_at"
                                 isValidDate={this.isValidDate}
                                 timeFormat={true}
-                                defaultValue={this.props.trip.start_at}
+                                defaultValue={tripData.start_at}
                                 inputProps={{name: 'start_at', id: 'start_at'}}
                                 labelClasses="register-form-label"
                                 wrapperClasses="register-form-birth_date"
@@ -116,11 +151,18 @@ class TripForm extends React.Component {
                     </div>
 
                     <div className={"form-group row " + (errors.luggage_size ? 'has-danger' : '')}>
-                        <label className="form-control-label text-muted col-sm-4" htmlFor="luggage_size">
+                        <label className="form-control-label text-muted col-sm-4"
+                            htmlFor="luggage_size"
+                        >
                             {translate('trip_form.luggage_size')}
                         </label>
                         <div className="col-sm-8">
-                            <select defaultValue={trip.luggage_size} name="luggage_size" className="form-control" id="luggage_size">
+                            <select
+                                name="luggage_size"
+                                id="luggage_size"
+                                className="form-control"
+                                defaultValue={tripData.luggage_size}
+                            >
                                 <option value="0">{translate('trip_form.luggage_size_0')}</option>
                                 <option value="1">{translate('trip_form.luggage_size_1')}</option>
                                 <option value="2">{translate('trip_form.luggage_size_2')}</option>
@@ -130,27 +172,33 @@ class TripForm extends React.Component {
                     </div>
 
                     <div className="form-group row">
-                        <label className="form-control-label text-muted col-sm-4" htmlFor="is_animals_allowed">
+                        <label className="form-control-label text-muted col-sm-4"
+                            htmlFor="is_animals_allowed"
+                        >
                             {translate('trip_form.is_animals_allowed')}
                         </label>
                         <div className="col-sm-8">
                             <div className="form-check">
                                 <label className="form-check-label">
-                                    <input className="form-check-input" type="checkbox" id="is_animals_allowed" defaultChecked={trip.is_animals_allowed} />
+                                    <input type="checkbox"
+                                        id="is_animals_allowed"
+                                        className="form-check-input"
+                                        defaultChecked={tripData.is_animals_allowed} />
                                 </label>
                             </div>
                         </div>
                     </div>
 
-                    <Waypoints waypoints={this.props.waypoints}
-                               placesCssClasses={this.props.placesCssClasses}
-                               onWaypointDelete={this.props.onWaypointDelete}
-                               onWaypointAdd={this.props.onWaypointAdd}
+                    <Waypoints waypoints={waypoints}
+                       placesCssClasses={placesCssClasses}
+                       onWaypointAdd={onWaypointAdd}
+                       onWaypointDelete={onWaypointDelete}
                     />
 
                     <div className="form-group">
                         <div className="text-center">
-                            <button type="submit" className="btn btn-primary">{this.props.trip ? translate('trip_form.edit_trip_btn') : translate('trip_form.create_trip_btn')}</button>
+                            <button type="submit" className="btn btn-primary">
+                                {buttonText}</button>
                         </div>
                     </div>
                 </div>
