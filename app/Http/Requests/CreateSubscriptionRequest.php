7badfb775d67d942760b9884362ca5f92c200920
Petro Ostapuk <petroostapuk@gmail.com>
2017-09-06 22:31:41 +0300

    Fixed style in my files #2

--- app/Http/Requests/CreateSubscriptionRequest.php
+++ app/Http/Requests/CreateSubscriptionRequest.php
@@ -40,7 +40,7 @@ class CreateSubscriptionRequest extends FormRequest implements CreateSubscriptio
      */
     public function getFrom(): array
     {
-        return (array)$this->get('start_point')['from'];
+        return (array) $this->get('start_point')['from'];
     }
 
     /**
@@ -48,7 +48,7 @@ class CreateSubscriptionRequest extends FormRequest implements CreateSubscriptio
      */
     public function getTo(): array
     {
-        return (array)$this->get('end_point')['to'];
+        return (array) $this->get('end_point')['to'];
     }
 
     /**
