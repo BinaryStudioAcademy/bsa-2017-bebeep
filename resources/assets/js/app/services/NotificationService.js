b0aef2978808f456d6119b11086cead848c98b0f
Vladimir <lenchvov@rambler.ru>
2017-09-10 10:30:35 +0300

    Changed system of locale notification

--- resources/assets/js/app/services/NotificationService.js
+++ resources/assets/js/app/services/NotificationService.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import {securedRequest} from './RequestService';
+import LangService from './LangService';
+import DateTimeHelper from 'app/helpers/DateTimeHelper'
 
 export const getNotifications = () => {
     return securedRequest.get('/api/v1/notifications');
@@ -12,4 +15,114 @@ export const markAsRead = (id) => {
 
 export const getCountUnread = () => {
     return securedRequest.get('/api/v1/notifications/unread/count');
+};
+
+export const getMessage = (notification) => {
+    const translate = LangService.translate;
+
+    switch (notification.type) {
+        case "booking_approved":
+            return {
+                type: 'success',
+                title: translate(
+                    `notifications.messages.${notification.type}.title`, {
+                        'from': notification.data.routes.from,
+                        'to': notification.data.routes.to,
+                    }
+                ),
+                message: translate(`notifications.messages.${notification.type}.message`, {
+                    'first_name': notification.data.driver.first_name,
+                    'last_name': notification.data.driver.last_name,
+                    'from': notification.data.routes.from,
+                    'to': notification.data.routes.to,
+                    'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
+                        onlyDate: true,
+                        dateFormat: 'MM.DD.YYYY'
+                    }).date})
+            };
+        case "booking_canceled":
+            return {
+                type: 'danger',
+                title: translate(
+                    `notifications.messages.${notification.type}.title`, {
+                        'from': notification.data.routes.from,
+                        'to': notification.data.routes.to,
+                    }
+                ),
+                message: translate(`notifications.messages.${notification.type}.message`, {
+                    'first_name': notification.data.user.first_name,
+                    'last_name': notification.data.user.last_name,
+                    'from': notification.data.routes.from,
+                    'to': notification.data.routes.to,
+                    'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
+                        onlyDate: true,
+                        dateFormat: 'MM.DD.YYYY'
+                    }).date})
+            };
+        case "booking_declined":
+            return {
+                type: 'danger',
+                title: translate(
+                    `notifications.messages.${notification.type}.title`, {
+                        'from': notification.data.routes.from,
+                        'to': notification.data.routes.to,
+                    }
+                ),
+                message: translate(`notifications.messages.${notification.type}.message`, {
+                    'first_name': notification.data.driver.first_name,
+                    'last_name': notification.data.driver.last_name,
+                    'from': notification.data.routes.from,
+                    'to': notification.data.routes.to,
+                    'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
+                        onlyDate: true,
+                        dateFormat: 'MM.DD.YYYY'
+                    }).date})
+            };
+        case "booking_created":
+            return {
+                type: 'warning',
+                title: translate(
+                    `notifications.messages.${notification.type}.title`, {
+                        'from': notification.data.routes.from,
+                        'to': notification.data.routes.to,
+                    }
+                ),
+                message: [(<div key="1">{
+                    translate(`notifications.messages.${notification.type}.message${LangService.getNumberForm(notification.data.booking.seats)}`, {
+                        'first_name': notification.data.user.first_name,
+                        'last_name': notification.data.user.last_name,
+                        'seats': notification.data.booking.seats,
+                        'from': notification.data.routes.from,
+                        'to': notification.data.routes.to,
+                        'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
+                            onlyDate: true,
+                            dateFormat: 'MM.DD.YYYY'
+                        }).date
+                    })}</div>), (<a key="2" className="btn" href={`/trips/?booking_trip=${notification.data.trip.trip_id}`}>
+                    {translate('notifications.messages.detail')}
+                </a>)]
+            };
+        case "review_on_trip_created":
+            return {
+                type: 'info',
+                title: translate(
+                    `notifications.messages.${notification.type}.title`, {
+                        'from': notification.data['trip']['from'],
+                        'to': notification.data['trip']['to'],
+                    }
+                ),
+                message: translate(`notifications.messages.${notification.type}.message`, {
+                    first_name: notification.data['review']['user']['first_name'],
+                    last_name: notification.data['review']['user']['last_name'],
+                    from: notification.data['trip']['from'],
+                    to: notification.data['trip']['to'],
+                })
+            };
+        default:
+            return {
+                type: '',
+                title: '',
+                message: '',
+            };
+    }
 };
--- resources/assets/js/features/notifications/components/NotificationsList.js
+++ resources/assets/js/features/notifications/components/NotificationsList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {getTranslate} from 'react-localize-redux';
-import {getNotifications} from 'app/services/NotificationService';
+import {getNotifications, getMessage} from 'app/services/NotificationService';
 import LangService from 'app/services/LangService';
 import {setNotifications} from '../actions'
-import { ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import Notification from './Notifications/Notification';
 import DateTimeHelper from 'app/helpers/DateTimeHelper'
 
@@ -18,91 +18,6 @@ class NotificationsList extends React.Component {
             .then(response => setNotifications(response.data.data));
     }
 
-    getMessage(notification) {
-        const {translate} = this.props;
-
-        switch (notification.type) {
-            case "booking_approved":
-                return {
-                    type: 'success',
-                    title: translate(`notifications.messages.${notification.type}.title`),
-                    message: translate(`notifications.messages.${notification.type}.message`, {
-                        'first_name': notification.data.driver.first_name,
-                        'last_name': notification.data.driver.last_name,
-                        'from': notification.data.routes.from,
-                        'to': notification.data.routes.to,
-                        'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
-                            onlyDate: true,
-                            dateFormat: 'MM.DD.YYYY'
-                        }).date})
-                };
-            case "booking_canceled":
-                return {
-                    type: 'danger',
-                    title: translate(`notifications.messages.${notification.type}.title`),
-                    message: translate(`notifications.messages.${notification.type}.message`, {
-                        'first_name': notification.data.user.first_name,
-                        'last_name': notification.data.user.last_name,
-                        'from': notification.data.routes.from,
-                        'to': notification.data.routes.to,
-                        'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
-                            onlyDate: true,
-                            dateFormat: 'MM.DD.YYYY'
-                        }).date})
-                };
-            case "booking_declined":
-                return {
-                    type: 'danger',
-                    title: translate(`notifications.messages.${notification.type}.title`),
-                    message: translate(`notifications.messages.${notification.type}.message`, {
-                        'first_name': notification.data.driver.first_name,
-                        'last_name': notification.data.driver.last_name,
-                        'from': notification.data.routes.from,
-                        'to': notification.data.routes.to,
-                        'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
-                            onlyDate: true,
-                            dateFormat: 'MM.DD.YYYY'
-                        }).date})
-                };
-            case "booking_created":
-                return {
-                    type: 'warning',
-                    title: translate(`notifications.messages.${notification.type}.title`),
-                    message: [(<div key="1">{
-                        translate(`notifications.messages.${notification.type}.message${LangService.getNumberForm(notification.data.booking.seats)}`, {
-                            'first_name': notification.data.user.first_name,
-                            'last_name': notification.data.user.last_name,
-                            'seats': notification.data.booking.seats,
-                            'from': notification.data.routes.from,
-                            'to': notification.data.routes.to,
-                            'start_at': DateTimeHelper.dateFormat(notification.data.trip.start_at, {
-                                onlyDate: true,
-                                dateFormat: 'MM.DD.YYYY'
-                            }).date
-                        })}</div>), (<a key="2" className="btn" href={`/trips/?booking_trip=${notification.data.trip.trip_id}`}>
-                            {translate('notifications.messages.detail')}
-                        </a>)]
-                };
-            case "review_on_trip_created":
-                return {
-                    type: 'info',
-                    title: translate(`notifications.messages.${notification.type}.title`),
-                    message: translate(`notifications.messages.${notification.type}.message`, {
-                        first_name: notification.data['review']['user']['first_name'],
-                        last_name: notification.data['review']['user']['last_name'],
-                        from: notification.data['trip']['from'],
-                        to: notification.data['trip']['to'],
-                    })
-                };
-            default:
-                return {
-                    type: '',
-                    title: '',
-                    message: '',
-                };
-        }
-    }
-
     render() {
         const {translate, notificationList, notifications} = this.props;
 
@@ -110,7 +25,7 @@ class NotificationsList extends React.Component {
             <ListGroup>
                     {notificationList.map((id) => {
                         const notification = notifications.byId[id],
-                            message = this.getMessage(notification);
+                            message = getMessage(notification);
 
                         return (
                             <ListGroupItem key={id} className="notification-item">
