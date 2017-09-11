fa179690417be867eee087125d3db9836944fb7b
Petro Ostapuk <petroostapuk@gmail.com>
2017-09-11 15:09:39 +0300

    Fixed date format

--- app/Notifications/BookingApproved.php
+++ app/Notifications/BookingApproved.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use Illuminate\Support\Facades\App;
 use Carbon\Carbon;
 use App\Models\Booking;
 use Illuminate\Bus\Queueable;
@@ -17,6 +18,12 @@ class BookingApproved extends Notification implements ShouldQueue
      * @var Booking
      */
     private $booking;
+    private $monthRu = array(1 => 'Января', 2 => 'Февраля', 3 => 'Марта', 4 => 'Апреля',
+        5 => 'Мая', 6 => 'Июня', 7 => 'Июля', 8 => 'Августа',
+        9 => 'Сентября', 10 => 'Октября', 11 => 'Ноября', 12 => 'Декабря');
+    private $monthUa = array(1 => 'Січня', 2 => 'Лютого', 3 => 'Березня', 4 => 'Квітня',
+        5 => 'Травня', 6 => 'Червня', 7 => 'Липня', 8 => 'Серпня',
+        9 => 'Вересня', 10 => 'Жовтня', 11 => 'Листопада', 12 => 'Грудня');
 
     /**
      * BookingApproved constructor.
@@ -31,7 +38,7 @@ class BookingApproved extends Notification implements ShouldQueue
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -42,7 +49,7 @@ class BookingApproved extends Notification implements ShouldQueue
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
@@ -53,8 +60,8 @@ class BookingApproved extends Notification implements ShouldQueue
 
         return (new MailMessage)
             ->markdown('emails.booking-approved', [
-                'trip_id' =>$data['trip']['trip_id'],
-                'date' => Carbon::createFromTimestampUTC($data['trip']['start_at']),
+                'trip_id' => $data['trip']['trip_id'],
+                'date' => $this->getDate($notifiable),
                 'from' => $from,
                 'to' => $to,
             ])
@@ -67,11 +74,61 @@ class BookingApproved extends Notification implements ShouldQueue
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
     {
         return fractal()->item($this->booking, new BookingTransformer())->toArray()['data'];
+    }
+
+    /**
+     * @param $notifiable
+     * @return string
+     */
+    public function getDate($notifiable)
+    {
+        $data = $this->toArray($notifiable);
+
+        switch (App::getLocale()) {
+            case 'uk': {
+                return $this->getMonthLocale($notifiable, $this->monthUa);
+            }
+                break;
+            case 'ru': {
+                return $this->getMonthLocale($notifiable, $this->monthRu);
+            }
+                break;
+            case 'en': {
+                $date = Carbon::createFromTimestampUTC($data['trip']['start_at'])->format('jS F Y');
+                $time = Carbon::createFromTimestampUTC($data['trip']['start_at'])->format('ha');
+                return $date . " at " . $time;
+            }
+                break;
+        }
+    }
+
+    /**
+     * @param array $month
+     * @param int $numberMonth
+     * @return string
+     */
+    private function getMonth(array $month, int $numberMonth): string
+    {
+        return $month[$numberMonth];
+    }
+
+    /**
+     * @param $notifiable
+     * @param array $localeMonth
+     * @return string
+     */
+    private function getMonthLocale($notifiable, array $localeMonth)
+    {
+        $data = $this->toArray($notifiable);
+        $numberMonth = Carbon::createFromTimestampUTC($data['trip']['start_at'])->month;
+        $month = $this->getMonth($localeMonth, $numberMonth);
+
+        return Carbon::createFromTimestampUTC($data['trip']['start_at'])->format('jго ' . $month . ' Y в H:i');
     }
 }
