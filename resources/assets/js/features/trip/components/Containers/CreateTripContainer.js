27e35f59574a568f95c64ce9b345d11cea7eaad4
Vladimir <lenchvov@rambler.ru>
2017-09-11 20:52:10 +0300

    Fixed

--- resources/assets/js/features/trip/components/Containers/CreateTripContainer.js
+++ resources/assets/js/features/trip/components/Containers/CreateTripContainer.js
@@ -174,8 +174,8 @@ class CreateTripContainer extends React.Component {
                                    waypoints={convertWaypointsToGoogleWaypoints(this.props.waypoints)}
                                    from={getCoordinatesFromPlace(this.state.startPoint.place)}
                                    to={getCoordinatesFromPlace(this.state.endPoint.place)}
-                                   fromData={this.state.endPoint}
-                                   toData={this.state.endPoint}
+                                   fromData={this.state.startPoint.place}
+                                   toData={this.state.endPoint.place}
                                    endTime={this.setEndTime.bind(this)}
                                    show={true}
                     />
