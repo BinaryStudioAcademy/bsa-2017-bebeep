e2b7a68f846ceb3357bf0f66467801fefaa1cf28
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-09-11 22:48:25 +0300

    basic info finished

--- resources/assets/js/features/search/layouts/MainPageContent.js
+++ resources/assets/js/features/search/layouts/MainPageContent.js
@@ -19,13 +19,55 @@ export default localize(class MainPageContent extends React.Component {
                     </div>
                     <div className="row">
                         <div className="col-md-4 p-2">
-                            <div className="main-page__block-step main-page__block-step-one"></div>
+                            <div className="main-page__block-step">
+                                <div className="main-page__block-step-one">
+                                    <span className="mb-2">{translate('main_page_content.step-one.main-text')}</span>
+                                    <div className="row mt-4">
+                                        <div className="col-md-2">
+                                            <div className="main-page__block-step-one-ico"></div>
+                                        </div>
+                                        <div className="col-md-10 pr-2">
+                                            <div className="main-page__block-step-one-secondary-text">
+                                                {translate('main_page_content.step-one.secondary-text')}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-md-4 p-2">
-                            <div className="main-page__block-step main-page__block-step-two"></div>
+                            <div className="main-page__block-step">
+                                <div className="main-page__block-step-two">
+                                    <span className="mb-2">{translate('main_page_content.step-two.main-text')}</span>
+                                    <div className="row mt-4">
+                                        <div className="col-md-2 pr-0">
+                                            <div className="main-page__block-step-two-ico"></div>
+                                        </div>
+                                        <div className="col-md-10 pl-0">
+                                            <div className="main-page__block-step-two-secondary-text">
+                                                {translate('main_page_content.step-two.secondary-text')}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-md-4 p-2">
-                            <div className="main-page__block-step main-page__block-step-three"></div>
+                            <div className="main-page__block-step">
+                                <div className="main-page__block-step-three">
+                                    <span className="mb-2">{translate('main_page_content.step-three.main-text')}</span>
+                                    <div className="row mt-4">
+                                        <div className="col-md-2">
+                                            <div className="main-page__block-step-three-ico"></div>
+                                        </div>
+                                        <div className="col-md-10 pr-2">
+                                            <div className="main-page__block-step-three-secondary-text">
+                                                {translate('main_page_content.step-three.secondary-text')}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
