955978eb8d7e2e702812a690a0deefb03668fcf7
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-12 13:07:34 +0300

    Changed transfer locale

--- resources/assets/js/features/search/components/Result/Dropdowns/TransferDropDown.js
+++ resources/assets/js/features/search/components/Result/Dropdowns/TransferDropDown.js
@@ -10,12 +10,12 @@ class TransferDropDown extends React.Component {
             <div className="filter__prop">
                 <div className="filter__prop-control">
                     <div className="filter__prop-name subscribe-modal-name">
-                        {translate('dropdown.transfer')}
+                        {translate('dropdown.transfer.name')}
                     </div>
 
                     <select name="transfer" value={value || ''} className="form-control" id="transfer" onChange={onChange}>
-                        <option value="false">{translate('dropdown.transfer1')}</option>
-                        <option value="true">{translate('dropdown.transfer2')}</option>
+                        <option value="false">{translate('dropdown.transfer.without')}</option>
+                        <option value="true">{translate('dropdown.transfer.with')}</option>
                     </select>
                 </div>
             </div>
