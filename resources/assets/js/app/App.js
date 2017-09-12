ab100cf757644441dc3ae38586ebc4743ec1a307
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-13 00:36:17 +0300

    Fixed

--- resources/assets/js/app/App.js
+++ resources/assets/js/app/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import Preloader from './components/Preloader';
 import MainHeader from './components/MainHeader';
 import Notifications from './components/Notifications';
 
 import AuthService from './services/AuthService';
+
+import { getCountUnread } from './services/NotificationService';
+import { setCountUnreadNotifications } from 'features/notifications/actions';
 
 import './bootstrap/bootstrap.scss';
 import './bootstrap/font-awesome.scss';
@@ -19,22 +24,55 @@ class App extends React.Component {
             isShowContent: false,
         }
 
+        this.onAuthSuccess = this.onAuthSuccess.bind(this);
+        this.onAuthError = this.onAuthError.bind(this);
         this.setShowContent = this.setShowContent.bind(this);
     }
 
     componentWillMount() {
-        if (AuthService.isAuthorized() || !AuthService.isSessionTokenValid()) {
+        if (AuthService.isAuthorized()) {
+            this.onAuthSuccess();
+            return;
+        }
+
+        if (!AuthService.isSessionTokenValid()) {
             this.setShowContent();
             return;
         }
 
-        AuthService.getSessionDataFromServer(this.setShowContent);
+        AuthService.getSessionDataFromServer(this.onAuthSuccess, this.onAuthError);
+    }
+
+    componentWillReceiveProps() {
+        if (AuthService.isAuthorized()) {
+            this.onAuthSuccess();
+            return;
+        }
+
+        if (!AuthService.isSessionTokenValid()) {
+            this.setShowContent();
+            return;
+        }
     }
 
     setShowContent() {
         this.setState({
             isShowContent: true,
         });
+    }
+
+    onAuthSuccess() {
+        const { setCountUnreadNotifications } = this.props;
+
+        this.setShowContent();
+
+        getCountUnread().then(response => {
+            setCountUnreadNotifications(response.data);
+        });
+    }
+
+    onAuthError() {
+        this.setShowContent();
     }
 
     renderComponent() {
@@ -56,4 +94,8 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default connect(
+    state => ({}),
+    dispatch =>
+        bindActionCreators({ setCountUnreadNotifications }, dispatch)
+)(App);
