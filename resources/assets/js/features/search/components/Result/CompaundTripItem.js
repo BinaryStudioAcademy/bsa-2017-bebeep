652487079c0dc5e1d1477cd655dcccb01c772800
Petro Ostapuk <petroostapuk@gmail.com>
2017-09-13 16:24:27 +0300

    Edit componnetn

--- resources/assets/js/features/search/components/Result/CompaundTripItem.js
+++ resources/assets/js/features/search/components/Result/CompaundTripItem.js
@@ -29,70 +29,11 @@ class TripItem extends React.Component {
                 <div className="row search-trip-item-block">
                     <div className="search-trip-item__user-container col-sm-4">
 
-                        <img className="search-trip-item__user-photo"
-                             src={ getDriverAvatar(trip.driver.data) }
-                             alt={ trip.driver.data.full_name }
-                        />
 
-                        <div className="search-trip-item__user-name"
-                             title={ trip.driver.data.full_name }
-                        >
-                            { trip.driver.data.full_name }
-                        </div>
-
-                        <div className="search-trip-item__user-age">
-                            { translate('search_result.years' + LangService.getNumberForm(
-                                trip.driver.data.age
-                            ), { age: trip.driver.data.age }) }
-                        </div>
                     </div>
 
                     <div className="search-trip-item__trip-container col-sm-8 clearfix">
-                        <div className="search-trip-item__description">
 
-                            <div className="search-trip-item__start-date">{ startedAt }</div>
-
-                            <div className="search-trip-item__route">
-                                { trip.routes.data.map((route) =>
-                                    <span className={"search-trip-item__route-item" +
-                                    (route.wanted ? ' search-trip-item__route-item_selected' : '')
-                                    } key={ route.point }>
-                                        { route.point }
-                                    </span>
-                                ) }
-                            </div>
-
-                            <div className="search-trip-item__from">
-                                <span className="search-trip-item__from-ico" />{
-                                trip.from.data.point }
-                            </div>
-                            <div className="search-trip-item__to">
-                                <span className="search-trip-item__from-ico search-trip-item__from-end" />{
-                                trip.to.data.point
-                            }
-                            </div>
-                        </div>
-
-                        <div className="search-trip-item__offer">
-
-                            <div className="search-trip-item__price">
-                                { parseInt(trip.price) }
-                                <span className="search-trip-item__price-currency">$</span>
-                            </div>
-
-                            <div className="search-trip-item__price-sign">
-                                { translate('search_result.per_passenger') }
-                            </div>
-
-                            <div className="search-trip-item__free-seats">
-                                <span className="search-trip-item__free-seats-text">
-                                    { trip.seats }
-                                </span> {
-                                translate('dropdown.free_seats.count_' + LangService.getNumberForm(
-                                    trip.seats
-                                )) }
-                            </div>
-                        </div>
                     </div>
                 </div>
             </Link>
