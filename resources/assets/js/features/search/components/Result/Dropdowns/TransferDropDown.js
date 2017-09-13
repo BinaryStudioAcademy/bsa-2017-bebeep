eee1e611d2f35ba09a5fd0af183d3157b641d60e
Petro Ostapuk <petroostapuk@gmail.com>
2017-09-13 13:52:09 +0300

    Added new item to dropdown

--- resources/assets/js/features/search/components/Result/Dropdowns/TransferDropDown.js
+++ resources/assets/js/features/search/components/Result/Dropdowns/TransferDropDown.js
@@ -15,7 +15,10 @@ class TransferDropDown extends React.Component {
 
                     <select name="transfer" value={value || ''} className="form-control" id="transfer" onChange={onChange}>
                         <option value="false">{translate('dropdown.transfer.without')}</option>
-                        <option value="true">{translate('dropdown.transfer.with')}</option>
+                        <option value="1">{translate('dropdown.transfer.with1')}</option>
+                        <option value="2">{translate('dropdown.transfer.with2')}</option>
+                        <option value="3">{translate('dropdown.transfer.with3')}</option>
+                        <option value="4">{translate('dropdown.transfer.with4')}</option>
                     </select>
                 </div>
             </div>
