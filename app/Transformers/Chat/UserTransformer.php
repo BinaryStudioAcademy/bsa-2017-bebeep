d53102f73847c5712f4265af682e3a969ab8daaf
Vladimir <lenchvov@rambler.ru>
2017-09-14 18:25:50 +0300

    Fixed

--- app/Transformers/Chat/UserTransformer.php
+++ app/Transformers/Chat/UserTransformer.php
@@ -9,15 +9,13 @@ class UserTransformer extends TransformerAbstract
 {
     public function transform(User $user)
     {
-        $birthDate = $user->birth_date;
-
         return [
             'id' => $user->id,
             'first_name' => $user->first_name,
             'last_name' => $user->last_name,
             'permissions' => $user->permissions,
             'birth_date' => $user->birth_date,
-            'birth_date_x' => $birthDate ?? $user->birth_date->timestamp,
+            'birth_date_x' => $user->birth_date ?? $user->birth_date->timestamp,
             'avatar' => $user->getAvatarUrl(),
         ];
     }
