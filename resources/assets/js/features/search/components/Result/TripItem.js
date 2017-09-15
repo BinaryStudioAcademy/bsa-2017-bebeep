a7243b2dd237897b8dec426f04d39713036195fe
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-16 00:29:23 +0300

    Used global user.age

--- resources/assets/js/features/search/components/Result/TripItem.js
+++ resources/assets/js/features/search/components/Result/TripItem.js
@@ -41,7 +41,7 @@ class TripItem extends React.Component {
                         </div>
 
                         <div className="search-trip-item__user-age">
-                            { translate('search_result.years', { age: trip.driver.data.age }) }
+                            { translate('user.age', { age: trip.driver.data.age }) }
                         </div>
                     </div>
 
