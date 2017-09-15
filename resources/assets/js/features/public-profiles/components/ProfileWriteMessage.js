42a997241ff45359e7cf6d132e5cfae81db29d54
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-15 23:32:03 +0300

    Used WriteMsgButton on public user profile page

--- resources/assets/js/features/public-profiles/components/ProfileWriteMessage.js
+++ resources/assets/js/features/public-profiles/components/ProfileWriteMessage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { localize } from 'react-localize-redux';
 
-import { LinkButton } from 'app/components/Buttons';
+import WriteMsgButton, { isEmptyWriteMsgButton } from 'app/components/Buttons/WriteMsgButton';
 
 import "../styles/public-profile.scss";
 
 class ProfileWriteMessage extends React.Component {
 
     render() {
-        const { translate, id } = this.props,
-            pathTo = `/dashboard/messages/${id}`;
+        const { translate, id } = this.props;
+
+        if (isEmptyWriteMsgButton(id)) {
+            return null;
+        }
 
         return (
             <div className="driver-car-block">
@@ -17,9 +20,7 @@ class ProfileWriteMessage extends React.Component {
                     <strong>{ translate('public_profile.write_message') } </strong>
                 </p>
 
-                <LinkButton pathTo={pathTo}
-                    id="write_msg_to_user"
-                    iconClassName="fa-envelope"
+                <WriteMsgButton userId={id}
                     className="w-100 py-2"
                     tooltip={false}
                 />
