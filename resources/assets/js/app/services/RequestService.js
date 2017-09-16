d43d0ed824d20a7b315a30f31130f4bacbc9ffbd
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-16 21:39:00 +0300

    Added checkInternalServerError

--- resources/assets/js/app/services/RequestService.js
+++ resources/assets/js/app/services/RequestService.js
@@ -18,11 +18,39 @@ const setAuthHeaders = (config) => {
     return config;
 };
 
+const checkInternalServerError = (error) => {
+    const status = ('' + error.response.status)[0];
+
+    if (status === '5') {
+        error.response.data = LangService.translate('errors.5xx_internal_server');
+    }
+};
+
 const simpleRequest = axios.create(requestParams);
 const securedRequest = axios.create(requestParams);
 
 securedRequest.interceptors.request.use(setAuthHeaders, null);
 securedRequest.interceptors.request.use(setLangHeaders, null);
 simpleRequest.interceptors.request.use(setLangHeaders, null);
+
+simpleRequest.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+
+}, function (error) {
+    checkInternalServerError(error);
+
+    return Promise.reject(error);
+});
+
+securedRequest.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+
+}, function (error) {
+    checkInternalServerError(error);
+
+    return Promise.reject(error);
+});
 
 export { simpleRequest, securedRequest };
