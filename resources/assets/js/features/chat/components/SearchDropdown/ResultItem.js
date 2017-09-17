ffe97c52187be956809946155ac7236486b7a6a5
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-17 18:48:49 +0300

    Added search ResultItem

---
+++ resources/assets/js/features/chat/components/SearchDropdown/ResultItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+
+import { getProfileAvatar } from 'app/services/PhotoService';
+
+class ResultItem extends React.Component {
+
+    render() {
+        const {option, onSearchResultItemClick} = this.props,
+            user = option;
+
+        return (
+            <li>
+                <Link to={`/dashboard/messages/${user.id}`} className="search-users__user-link">
+                    <img
+                        src={getProfileAvatar(user.avatar)}
+                        style={{
+                            width: '30px',
+                            marginRight: '10px',
+                            borderRadius: '50%',
+                        }}
+                    />
+                    <span>{user.first_name} {user.last_name}</span>
+                </Link>
+            </li>
+        );
+    }
+}
+
+export default ResultItem;
