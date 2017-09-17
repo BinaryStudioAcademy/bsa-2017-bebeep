88ee4fbd614e6ac49ad2fd3c75cbcdcf254639f7
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-17 12:55:51 +0300

    Added AsyncTypeahead to SearchUsers component, changed code

--- resources/assets/js/features/chat/components/SearchUsers.js
+++ resources/assets/js/features/chat/components/SearchUsers.js
@@ -1,57 +1,108 @@
 import React from 'react';
-import {bindActionCreators} from 'redux';
+import {browserHistory} from 'react-router';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import {getTranslate} from 'react-localize-redux';
+import {AsyncTypeahead} from 'react-bootstrap-typeahead';
+
 import {filterUsers} from '../actions';
+import {getProfileAvatar} from 'app/services/PhotoService';
+
 import '../styles/search-users.scss';
 
+
 class SearchUsers extends React.Component {
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
 
         this.state = {
-            preloader: false
+            preloader: false,
+            options: [],
         };
 
         this.filterUsers = this.filterUsers.bind(this);
+        this.renderMenuItemChildren = this.renderMenuItemChildren.bind(this);
+        this.onSearchResultItemClick = this.onSearchResultItemClick.bind(this);
     }
 
-    filterUsers(e) {
+    filterUsers(query) {
         this.setState({preloader: true});
-        this.props.filterUsers(e.target.value)
-            .then(() => {
-                this.setState({preloader: false});
+
+        this.props.filterUsers(query)
+            .then(response => {
+                this.setState({
+                    preloader: false,
+                    options: response,
+                });
             })
-            .catch(() => {
-                this.setState({preloader: false});
+            .catch(error => {
+                this.setState({
+                    preloader: false,
+                    options: [],
+                });
             });
     }
 
+    onSearchResultItemClick(userId) {
+        browserHistory.push(`/dashboard/messages/${userId}`);
+    }
+
+    renderMenuItemChildren(user, props, index) {
+        return (
+            <div key={user.id} onClick={() => this.onSearchResultItemClick(user.id)}>
+                <img
+                    src={getProfileAvatar(user.avatar)}
+                    style={{
+                        width: '30px',
+                        marginRight: '10px',
+                        borderRadius: '50%',
+                    }}
+                />
+                <span>{user.first_name} {user.last_name}</span>
+            </div>
+        );
+    }
+
     render() {
-        const {translate} = this.props,
-            {preloader} = this.state,
+        const { translate } = this.props,
+            { preloader, options } = this.state,
             icoClass = "search-users__icon fa" + (
                 preloader ? " fa-circle-o-notch fa-spin" : " fa-search"
             );
 
-        return (
-            <div className="input-group search-users">
-                <div className="input-group-addon search-users__icon-wrapper">
+        /*<div className="input-group-addon search-users__icon-wrapper">
                     <i className={icoClass} />
                 </div>
                 <input type="text"
                     className="form-control search-users__input"
                     placeholder={translate('chat.user_list.search_placeholder')}
                     onChange={this.filterUsers}
-                />
-            </div>
+                />*/
+
+        return (
+            <AsyncTypeahead
+                options={options}
+                className="bootstrap-typeahead"
+                filterBy={['first_name', 'last_name']}
+                labelKey={option => `${option.first_name} ${option.last_name}`}
+                onSearch={this.filterUsers}
+                placeholder={translate('chat.user_list.search_placeholder')}
+                renderMenuItemChildren={this.renderMenuItemChildren}
+                promptText="Type to search..."
+                searchText="Searching..."
+                minLength={1}
+                multiple={false}
+                useCache={false}
+                delay={200}
+            />
         );
     }
 }
 
 export default connect(
     state => ({
-        translate: getTranslate(state.locale)
+        translate: getTranslate(state.locale),
     }),
     dispatch => bindActionCreators({filterUsers}, dispatch)
 )(SearchUsers);
