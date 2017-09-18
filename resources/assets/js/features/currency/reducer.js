c10bd46461b738bb4240b369ae370f6ef7ad945c
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-18 18:28:47 +0300

    Added space

--- resources/assets/js/features/currency/reducer.js
+++ resources/assets/js/features/currency/reducer.js
@@ -7,6 +7,7 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch(action.type) {
+
         case actions.SET_CURRENCIES:
             return {
                 currencies: action.payload.currencies,
