d816c1ab6153496adc0bba046703193fe028ab42
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-18 20:51:47 +0300

    Added Currency model

---
+++ app/Models/Currency.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'rate',
+        'is_main',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'is_main' => 'boolean',
+    ];
+}
