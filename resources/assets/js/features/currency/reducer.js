172c5b5873ebd3722149ade5a2369682a7b67f56
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-18 18:28:02 +0300

    Added the currency reducer

---
+++ resources/assets/js/features/currency/reducer.js
@@ -0,0 +1,25 @@
+import * as actions from './actionTypes';
+
+const initialState = {
+    currencies: [],
+    activeCurrency: null,
+};
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case actions.SET_CURRENCIES:
+            return {
+                currencies: action.payload.currencies,
+                activeCurrency: action.payload.activeCurrency,
+            };
+
+        case actions.SET_ACTIVE_CURRENCE:
+            return {
+                ...state,
+                activeCurrency: action.payload.activeCurrency,
+            };
+
+        default:
+            return state;
+    }
+}
