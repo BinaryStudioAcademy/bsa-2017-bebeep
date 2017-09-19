757357a0d0912d46cff70b3a444d396a0205aa67
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-19 16:53:29 +0300

    Added getCurrencies, changed code

--- resources/assets/js/features/currency/actions.js
+++ resources/assets/js/features/currency/actions.js
@@ -1,4 +1,5 @@
 import * as actions from './actionTypes';
+import { securedRequest } from 'app/services/RequestService';
 
 export const setCurrencies = payload => ({
     type: actions.SET_CURRENCIES,
@@ -9,3 +10,19 @@ export const setActiveCurrency = payload => ({
     type: actions.SET_ACTIVE_CURRENCY,
     payload,
 });
+
+export const getCurrencies = () => dispatch => {
+    return new Promise((success, error) => {
+        securedRequest.get('/api/v1/currencies')
+            .then(response => {
+                dispatch(setCurrencies(response.data));
+                success();
+            })
+            .catch(error => {
+                if (typeof error !== 'function') {
+                    return;
+                }
+                error();
+            });
+    });
+};
