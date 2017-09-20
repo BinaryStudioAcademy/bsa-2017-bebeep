3ddf38654fe8c6765d51b1528f1f415d7cbd7015
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-20 23:32:32 +0300

    Added transfersChange to constructor init

--- resources/assets/js/features/search/components/Result/Filter.js
+++ resources/assets/js/features/search/components/Result/Filter.js
@@ -33,7 +33,7 @@ class Filter extends React.Component {
         this.luggageChange = this.luggageChange.bind(this);
         this.seatsChange = this.seatsChange.bind(this);
         this.ratingChange = this.ratingChange.bind(this);
-        // this.transferChange = this.transferChange.bind(this);
+        this.transfersChange = this.transfersChange.bind(this);
     }
 
     componentWillMount() {
