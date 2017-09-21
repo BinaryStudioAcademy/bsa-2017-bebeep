cf71b83b3de4a6a32ec8df7ffa709465e499fa7b
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-21 17:08:53 +0300

    Added sortByPrice method

--- app/Services/Result/SearchTripCollection.php
+++ app/Services/Result/SearchTripCollection.php
@@ -6,23 +6,53 @@ use Illuminate\Support\Collection;
 
 class SearchTripCollection extends Collection
 {
+    /**
+     * @var array
+     */
     protected $meta;
 
-    public function __construct($items = [])
+    /**
+     * @param array $items
+     */
+    public function __construct(array $items = [])
     {
         parent::__construct($items);
     }
 
-    public function setMeta(array $meta)
+    /**
+     * @param array $items
+     *
+     * @return $this
+     */
+    public function setMeta(array $meta): self
     {
         $this->meta = $meta;
+
+        return $this;
     }
 
-    public function getMeta() : array
+    /**
+     * @return array
+     */
+    public function getMeta(): array
     {
         return [
             'total' => $this->meta['count'],
             'price' => ['min' => $this->meta['min'], 'max' => $this->meta['max']],
         ];
+    }
+
+    /**
+     * @param array $items
+     *
+     * @return \App\Services\Result\SearchTripCollection - new collection
+     */
+    public function sortByPrice(string $order = 'asc'): self
+    {
+        $method = $order === 'asc' ? 'sortBy' : 'sortByDesc';
+
+        return $this->$method(function ($trip) {
+            return $trip->priceInCurrency;
+        })->values();
     }
 }
