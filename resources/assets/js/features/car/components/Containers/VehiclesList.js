313986e862f9ad935670288aa02040f5fcf49887
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-09-22 18:30:03 +0300

    Changed VehiclesList

--- resources/assets/js/features/car/components/Containers/VehiclesList.js
+++ resources/assets/js/features/car/components/Containers/VehiclesList.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { localize } from 'react-localize-redux';
 
-import VehicleItem from '../Elements/VehicleItem';
+import { VehicleItem, VehicleActions } from '../Elements';
 import { getVehicles, deleteVehicle } from 'features/car/actions';
 
 class VehiclesList extends React.Component {
@@ -20,38 +20,68 @@ class VehiclesList extends React.Component {
         this.props.deleteVehicle(id);
     }
 
+    renderVehiclesItems() {
+        const { vehicles } = this.props;
+
+        if (vehicles.length <= 0) {
+            return null;
+        }
+
+        return vehicles.map(vehicle =>
+            <VehicleItem key={ vehicle.id } vehicle={ vehicle } />
+        );
+    }
+
+    renderVehiclesActions() {
+        const { vehicles } = this.props;
+
+        if (vehicles.length <= 0) {
+            return null;
+        }
+
+        return vehicles.map(vehicle =>
+            <VehicleActions key={ vehicle.id }
+                vehicle={ vehicle }
+                onDeleteVehicle={this.onDeleteVehicle} />
+        );
+    }
+
     render() {
         const { translate, vehicles } = this.props;
 
-        const vehiclesView = (vehicles.length > 0)
-            ? vehicles.map(vehicle => {
-                return (
-                    <VehicleItem key={ vehicle.id } vehicle={ vehicle }
-                        onDeleteVehicle={this.onDeleteVehicle} />
-                );
-            })
-            : null;
-
         return (
-            <div className="table-responsive">
-                <table className="table table-list-details">
-                    <thead className="with-background-teal">
-                        <tr>
-                            <th>{ translate('vehicles_list.options.brand') }</th>
-                            <th>{ translate('vehicles_list.options.model') }</th>
-                            <th>{ translate('vehicles_list.options.year') }</th>
-                            <th>{ translate('vehicles_list.options.body') }</th>
-                            <th>{ translate('vehicles_list.options.color') }</th>
-                            <th>{ translate('vehicles_list.options.seats') }</th>
-                            <th>{ translate('vehicles_list.options.photo') }</th>
-                            <th className="table-details__cell-for-action"></th>
-                            <th className="table-details__cell-for-action"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { vehiclesView }
-                    </tbody>
-                </table>
+            <div className="d-flex justify-content-between">
+                <div className="table-responsive">
+                    <table className="table table-list-details">
+                        <thead className="with-background-teal">
+                            <tr>
+                                <th>{ translate('vehicles_list.options.brand') }</th>
+                                <th>{ translate('vehicles_list.options.model') }</th>
+                                <th>{ translate('vehicles_list.options.year') }</th>
+                                <th>{ translate('vehicles_list.options.body') }</th>
+                                <th>{ translate('vehicles_list.options.color') }</th>
+                                <th>{ translate('vehicles_list.options.seats') }</th>
+                                <th>{ translate('vehicles_list.options.photo') }</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderVehiclesItems()}
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <table className="table table-list-details">
+                        <thead className="with-background-teal">
+                            <tr>
+                                <th className="table-details__cell-for-action">&nbsp;</th>
+                                <th className="table-details__cell-for-action">&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderVehiclesActions()}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
